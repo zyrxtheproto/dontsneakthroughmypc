28335628fefb6eaed2e6b53e948d25c2d1d279bf
zyrx <102766440+zyrxtheproto@users.noreply.github.com>
2024-02-04 22:23:21 +0100

    Updated with bug fixes

---
+++ noescape.py
@@ -0,0 +1,71 @@
+import requests
+import datetime
+import time
+from colorama import Fore, Style, init
+
+init(autoreset=True)
+
+def send_push_notification(api_key, title, body):
+    url = "https://api.pushbullet.com/v2/pushes"
+    headers = {
+        "Access-Token": api_key,
+        "Content-Type": "application/json"
+    }
+
+    # Get current date
+    current_date = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    # Include the date in the body of the notification
+    body = f"{body} - {current_date}"
+
+    data = {
+        "type": "note",
+        "title": title,
+        "body": body
+    }
+
+    response = requests.post(url, headers=headers, json=data)
+
+    if response.status_code == 200:
+        print("1")
+    else:
+        log_file_name = f"log_{datetime.datetime.now().strftime('%Y-%m-%d')}.txt"
+        with open(log_file_name, "a", encoding="utf-8") as log_file: 
+            log_file.write(f"Error: {response.text}\n")
+
+
+message = """
+                          ████████                          
+                        ██        ██                        
+                      ██            ██                      Warning - This program is made to prevent people
+                      ██            ██                      who shouldn't be using your computer from using it.
+                    ██    ████████    ██                    
+                  ██    ████████████    ██                  
+                  ██    ████████████    ██                  
+                ██      ████████████      ██                
+              ██          ████████          ██              
+              ██          ████████          ██              If you're right here, it means that you really shouldn't
+            ██            ████████            ██            continue using this computer, as everything you do is being
+          ██                ████                ██          monitored and will be reported to the owner of the computer.
+          ██                ████                ██          
+        ██                                        ██        
+      ██                    ████                    ██      
+      ██                  ████████                  ██      
+    ██                  ████████████                  ██    Goodbye, and just shut down the computer...
+  ██                    ████████████                    ██  
+  ██                      ████████                      ██  
+  ██                        ████                        ██  
+    ██                                                ██    
+      ████████████████████████████████████████████████    
+"""
+time.sleep(2)
+print(Fore.YELLOW + message)
+
+
+api_key = "InsertYourAPIKeyHere"
+send_push_notification(api_key, "Notification", "PC Started!")
+
+# wait until program is closed, if it isn't it will send a notification every 5 minutes
+while True:
+    time.sleep(300)
+    send_push_notification(api_key, "Notification", "PC still Running!")
---
+++ run.bat
@@ -0,0 +1,7 @@
+@echo off
+pip install requests
+pip install datetime
+pip install time
+title NoEscape User Protection
+cls
+python "ExactPathToPythonFileWithQuotationMarks"
